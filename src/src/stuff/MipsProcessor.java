99650552f6154e42f8b132f2f09cc6a646fe37df
sofiagadea11 <sofia.gadea.omelchenko@gmail.com>
2014-08-01 22:00:49 -0400

    Processor reads instruction ;) a sexy one

--- src/src/stuff/MipsProcessor.java
+++ src/src/stuff/MipsProcessor.java
@@ -1,9 +1,44 @@
 package stuff;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class MipsProcessor {
 
-    public static String giveHello(){
-        return "Hello World!";
+    Queue<String> instructions;
+    String currentInstruction;
+
+    public MipsProcessor(String instructionsFile){
+        instructions = new LinkedBlockingQueue<String>();
+
+        File file = new File(instructionsFile);
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                instructions.add(line);
+            }
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
+        }
     }
 
+    public void runCycleOne(){
+        setCurrentInstruction(instructions.remove());
+    }
+
+    public String getCurrentInstruction(){
+        return currentInstruction;
+    }
+
+    public void setCurrentInstruction(String newInstruction){
+        currentInstruction = newInstruction;
+    }
 }
--- src/test/stuff/ProcessorTests.java
+++ src/test/stuff/ProcessorTests.java
@@ -1,14 +1,25 @@
 package stuff;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ProcessorTests {
 
+    MipsProcessor myProcessor;
+
+    @Before
+    public void setup(){
+        myProcessor = new MipsProcessor("C:\\Users\\Owner\\Documents\\dev\\instructions.txt");
+    }
+
     @Test
-    public void processorSaysHello() {
-        assertEquals("Hello World!", MipsProcessor.giveHello());
+    public void duringCycleOneInstructionIsLoadedFromMemory(){
+        assertNull(myProcessor.getCurrentInstruction());
+        myProcessor.runCycleOne();
+        assertEquals("add $s1 $s3 $s3", myProcessor.getCurrentInstruction());
     }
 
 }
