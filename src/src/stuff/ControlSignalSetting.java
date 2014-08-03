95209a32a84a803857d2a851334167bb67c02bb0
sofiagadea11 <sofia.gadea.omelchenko@gmail.com>
2014-08-03 16:34:29 -0400

    Adding processor state tests

--- src/src/stuff/ControlSignalSetting.java
+++ src/src/stuff/ControlSignalSetting.java
@@ -28,4 +28,44 @@ public class ControlSignalSetting {
         this.aluSrcB = aluSrcB;
         this.pcSource = pcSource;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ControlSignalSetting that = (ControlSignalSetting) o;
+
+        if (aluOp != that.aluOp) return false;
+        if (aluSrcA != that.aluSrcA) return false;
+        if (aluSrcB != that.aluSrcB) return false;
+        if (iOrD != that.iOrD) return false;
+        if (irWrite != that.irWrite) return false;
+        if (memRead != that.memRead) return false;
+        if (memWrite != that.memWrite) return false;
+        if (memtoReg != that.memtoReg) return false;
+        if (pcSource != that.pcSource) return false;
+        if (pcWrite != that.pcWrite) return false;
+        if (regDst != that.regDst) return false;
+        if (regWrite != that.regWrite) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (regDst ? 1 : 0);
+        result = 31 * result + (regWrite ? 1 : 0);
+        result = 31 * result + (aluSrcA ? 1 : 0);
+        result = 31 * result + (memRead ? 1 : 0);
+        result = 31 * result + (memWrite ? 1 : 0);
+        result = 31 * result + (memtoReg ? 1 : 0);
+        result = 31 * result + (iOrD ? 1 : 0);
+        result = 31 * result + (irWrite ? 1 : 0);
+        result = 31 * result + (pcWrite ? 1 : 0);
+        result = 31 * result + aluOp;
+        result = 31 * result + aluSrcB;
+        result = 31 * result + pcSource;
+        return result;
+    }
 }
--- src/test/stuff/ProcessorStateTests.java
+++ src/test/stuff/ProcessorStateTests.java
@@ -12,4 +12,12 @@ public class ProcessorStateTests {
         assertEquals(ProcessorState.ONE, ProcessorState.ZERO.nextState(instruction));
     }
 
+    @Test
+    public void stateEightReturnsCorrectSetting(){
+        ControlSignalSetting beqSetting = new ControlSignalSetting(false,false,true,false,false,false,false,false,false,0,0,1);
+        assertEquals(beqSetting, ProcessorState.EIGHT.getSetting("BEQ"));
+        ControlSignalSetting addiSetting = new ControlSignalSetting(false,false,true,false,false,false,false,false,false,0,2,1);
+        assertEquals(addiSetting, ProcessorState.EIGHT.getSetting("ADDI"));
+    }
+
 }
